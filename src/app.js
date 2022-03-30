9994d210d85c425cc28626bba258073393600c7f
Phherten <phigueraherten@gmail.com>
2022-03-30 18:00:43 +0000

    imprimir en html

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 let pronombre = ["mi", "la", "una", "esta"];
 let adjetivo = ["gran", "fantastica", "pequeña", "increible"];
 let nombre = ["aventura", "escapada", "experiencia", "bicicleta"];
+let body = document.querySelector("body");
 
 for (let i = 0; i < pronombre.length; i++) {
   for (let x = 0; x < adjetivo.length; x++) {
     for (let y = 0; y < nombre.length; y++) {
       {
-        console.log(pronombre[i] + adjetivo[x] + nombre[y] + ".com");
+        body.innerHTML +=
+          "<p> *" + pronombre[i] + adjetivo[x] + nombre[y] + ".com <br></p>";
       }
     }
   }
